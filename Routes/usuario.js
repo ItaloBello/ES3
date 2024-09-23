83c8fa83782a36a8cf7d96284dfa2ba5a3572398
Italo <italo.bello10@gmail.com>
2024-09-23 09:38:05 -0300

    merge branch master to main

---
+++ Routes/usuario.js
@@ -0,0 +1,122 @@
+const express = require("express")
+const router = express.Router()
+const Usuario = require('../Models/Usuario')
+const bodyParser = require('body-parser')
+const { validarCPF, validarCNPJ } = require('../Utils/validarDocumento')
+const bcrypt = require('bcrypt')
+const { Op } = require('sequelize')
+const passport = require('passport')
+
+
+router.use(bodyParser.json());
+
+router.get("/registro", function(req, res) {
+    res.render("usuario/registro")
+});
+
+router.post("/registro", function(req, res) {
+    var erros = [];
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: "Nome inválido!" });
+    }
+
+    if (!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+        erros.push({ texto: "Email inválido!" });
+    }
+
+    if (!req.body.endereco || typeof req.body.endereco == undefined || req.body.endereco == null) {
+        erros.push({ texto: "Endereço inválido!" });
+    }
+
+    if (!req.body.dataNasc || typeof req.body.dataNasc == undefined || req.body.dataNasc == null) {
+        erros.push({ texto: "Data inválida!" });
+    }
+
+    if (req.body.senha.length < 8) {
+        erros.push({ texto: "Senha muito curta!(adicione no mínimo 8 caracteres)" });
+    }
+
+    if (req.body.senha != req.body.senha2) {
+        erros.push({ texto: "As senhas são diferentes!" });
+    }
+
+    // Validar o CPF
+    const documento = req.body.documento.replace(/\D/g, '');
+
+    if (documento.length === 11) {
+        if (!validarCPF(documento)) {
+            erros.push({ texto: "CPF inválido!" });
+        }
+    } else if (documento.length === 14) {
+        if (!validarCNPJ(documento)) {
+            erros.push({ texto: "CNPJ inválido!" });
+        }
+    } else {
+        erros.push({ texto: "Documento deve ter 11 ou 14 dígitos!" });
+    }
+
+    if (erros.length > 0) {
+        return res.render("usuario/registro", { erros }); 
+    } else {
+        // Verificando se já existe um usuário com o email ou CPF
+        Usuario.findOne({
+            where: {
+                [Op.or]: [{ email: req.body.email }, { documento: documento }]
+            }
+        }).then((usuario) => {
+            if (usuario) {
+                req.flash("error_msg", "Usuário já cadastrado com este email ou CPF!");
+                return res.redirect("/usuario/registro"); 
+            } else {
+                //Salvando o usuario
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    documento: documento,
+                    email: req.body.email,
+                    endereco: req.body.endereco,
+                    data_nascimento: req.body.dataNasc,
+                    senha: req.body.senha
+                });
+
+                // Hashing da senha
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if (erro) {
+                            req.flash("error_msg", "Houve um erro durante o salvamento");
+                            return res.redirect("/"); 
+                        }
+
+                        novoUsuario.senha = hash;
+
+                        novoUsuario.save().then(() => {
+                            req.flash("success_msg", "Usuário salvo com sucesso");
+                            res.redirect("/");
+                        }).catch((err) => {
+                            req.flash("error_msg", "Houve um erro durante a criação");
+                            res.redirect("/usuario/registro");
+                        });
+                    });
+                });
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno");
+            res.redirect("/");
+        });
+    }
+})
+
+
+// Start do login
+router.get("/login", (req,res) => {
+    res.render("usuario/login")
+})
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/usuario/login",
+    failureFlash: true
+}));
+
+// Exportando o módulo
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,85 @@
+//Carregando os módulos
+require('dotenv').config();
+const express = require('express')
+const { engine }  = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const sequelize = require('./database');
+const adminRoute = require('./Routes/admin')
+const userRoute = require('./Routes/usuario')
+const path = require('path')
+const Usuario = require('./Models/Usuario')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+require('./Config/auth')(passport)
+
+
+
+
+//Config
+
+    // Sessão
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    //Flash
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use(flash());
+    
+
+    // Middleware para configurar variáveis globais para o `flash`
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg');
+        res.locals.error_msg = req.flash('error_msg');
+        res.locals.error = req.flash('error')[0]; 
+        next();
+    });
+
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    //HandleBars
+        app.engine('handlebars', engine({defaultLayout: 'main'}))    
+        app.set('view engine', 'handlebars');
+
+    //MySql -> database.js
+
+
+
+    //Path
+        app.set('views', path.join(__dirname, 'views'));
+        app.use(express.static(path.join(__dirname, 'Public')))
+    
+    
+// Teste de Conexão
+sequelize.authenticate()
+    .then(function() {
+        console.log('Conexão com o MySQL estabelecida com sucesso.');
+        
+        // Criando a tabela(deixar comentado o force dps de criar para criar 1x só)
+        Usuario.sync(/*{ force: true }*/)
+    })
+    .then(function() {
+
+//Rotas
+        //ADM
+        app.use('/admin', adminRoute);
+        //USUARIOS
+        app.use('/usuario', userRoute);
+})
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+    res.render('home'); 
+});
+
+//Outros
+const PORT = 8081
+app.listen(PORT, function(){
+    console.log("Servidor Rodando na porta" + PORT)
+})
